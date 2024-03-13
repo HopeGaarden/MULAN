0684b3a26bc5299beba0977569324463f7f9464f
이주성 <32183520@dankook.ac.kr>
2024-03-13 15:21:26 +0900

    test(#3): Jasypt 암호화, 복호화 테스트

---
+++ Back/src/test/java/com/example/back/config/JasyptConfigTest.java
@@ -0,0 +1,35 @@
+package com.example.back.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+
+import static com.example.back.config.IntegrationHelper.NON_ASCII;
+
+@SuppressWarnings(NON_ASCII)
+class JasyptConfigTest extends IntegrationHelper {
+    @Value("${jasypt.encryptor.password}")
+    private String key;
+
+    @Test
+    void 암호화() {
+        String value = "test";
+        System.out.println(jasyptEncoding(value));
+    }
+
+    @Test
+    void 복호화() {
+        String value = "nsHveCItHp8U6rPPdTWfUA==";
+        BasicTextEncryptor enc = new BasicTextEncryptor();
+        enc.setPassword(key);
+        System.out.println(enc.decrypt(value));
+    }
+
+    public String jasyptEncoding(String value) {
+        StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
+        enc.setAlgorithm("PBEWithMD5AndDES");
+        enc.setPassword(key);
+        return enc.encrypt(value);
+    }
+}
