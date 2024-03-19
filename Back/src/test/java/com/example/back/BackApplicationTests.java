2c48e8490376e272d82577f261eb32727cb343c4
이주성 <32183520@dankook.ac.kr>
2024-03-19 13:30:14 +0900

    feat(#17): BaseEntity 적용을 위해 JPA Auditing 기능 활성화

--- Back/src/test/java/com/example/back/BackApplicationTests.java
+++ Back/src/test/java/com/example/back/BackApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.back;
 
 import com.example.back.config.IntegrationHelper;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 기능 활성화 - BaseEntity
 class BackApplicationTests extends IntegrationHelper {
 
     @Test
