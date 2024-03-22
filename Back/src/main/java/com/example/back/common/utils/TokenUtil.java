c341d13b8082311255d530d369e4e538741aeec7
이주성 <32183520@dankook.ac.kr>
2024-03-23 00:46:57 +0900

    test(#24): JWT 토큰 프로바이더 테스트

---
+++ Back/src/main/java/com/example/back/common/utils/TokenUtil.java
@@ -0,0 +1,18 @@
+package com.example.back.common.utils;
+
+import com.example.back.domain.auth.member.Member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokenUtil {
+    public static Map<String, String> createTokenMap(Member member) {
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("role", String.valueOf(member.getRole()));
+        map.put("nickname", String.valueOf(member.getNickname()));
+        map.put("password", String.valueOf(member.getPassword()));
+
+        return map;
+    }
+}
